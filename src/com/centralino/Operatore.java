5957469d199c8b810bb50f457106a196ebd91bfb
admin <admin@PC1955>
2017-03-30 17:50:06 +0200

    "aggiustamento posizione barre"

--- src/com/centralino/Operatore.java
+++ src/com/centralino/Operatore.java
@@ -14,9 +14,9 @@ public class Operatore implements Runnable {
 	@Override
 	public void run() {
 		try{
-			System.out.println("Operatore " + counter + " sta servendo: " + cServito.nome + "\r\n --------------------------------------------");
+			System.out.println("Operatore " + counter + " sta servendo: " + cServito.nome + "\r\n--------------------------------------------");
 			Thread.sleep(2000);
-			System.out.println("Operatore " + counter + " ha finito di servire: " + cServito.nome + "\r\n --------------------------------------------");
+			System.out.println("Operatore " + counter + " ha finito di servire: " + cServito.nome + "\r\n--------------------------------------------");
 			contrOper.DecrementaOpOccupati();
 		}catch(InterruptedException e){
 			e.printStackTrace();
